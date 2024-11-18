deb517acfd95ff0a2c2e00b1282a609a9863c702
barkeA <b.abeid@hotmail.co.uk>
2024-11-18 15:31:11 +0000

    file setup

---
+++ Gazetteer/libs/js/script.js
@@ -0,0 +1,56 @@
+// ---------------------------------------------------------
+// GLOBAL DECLARATIONS
+// ---------------------------------------------------------
+
+var map = L.map('backgroundMap', {
+    "tap": false
+}).setView([50, 0], 3);
+
+// tile layers
+
+var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
+  }
+);
+
+var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+  }
+);
+
+var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var basemaps = {
+  "Streets": streets,
+  "Satellite": satellite
+};
+
+// buttons
+
+var infoBtn = L.easyButton("fa-info fa-xl", function (btn, map) {
+  $("#exampleModal").modal("show");
+});
+
+// ---------------------------------------------------------
+// EVENT HANDLERS
+// ---------------------------------------------------------
+
+// initialise and add controls once DOM is ready
+
+$(document).ready(function () {
+  
+  map = L.map("map", {
+    layers: [streets]
+  }).setView([54.5, -4], 6);
+  
+  // setView is not required in your application as you will be
+  // deploying map.fitBounds() on the country border polygon
+
+  layerControl = L.control.layers(basemaps).addTo(map);
+
+  infoBtn.addTo(map);
+
+})
